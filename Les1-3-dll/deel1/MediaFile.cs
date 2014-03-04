016b44109a65d2e84c8534ffb6f9c145cae8abcb
Dries Christiaens <dries.christiaens@student.Ehb.Be>
2014-03-04 15:52:57 +0100

    Mediafile aanpassing + maand

--- Les1-3-dll/deel1/MediaFile.cs
+++ Les1-3-dll/deel1/MediaFile.cs
@@ -39,29 +39,34 @@ namespace Les1_3_dll
             StringBuilder builder = new StringBuilder();
             builder.AppendLine("Bestand: " + fileInfo.Name);
             builder.AppendLine("FileType: " + MediaType);
-
-            TimeSpan dateAccess = DateTime.Now.Subtract(fileInfo.LastAccessTime);
-            string appendTime = "";
-
-            if(dateAccess.Days > 0)
-            {
-                appendTime = dateAccess.Days + " dagen geleden";
-            }
-            else if (dateAccess.Hours > 0)
-            {
-                appendTime = dateAccess.Hours + " u(u)r(en) geleden";
-            }
-            else if (dateAccess.Minutes > 0)
-            {
-                appendTime = dateAccess.Minutes + " minuten geleden";
-            }
-            else
-            {
-                appendTime = dateAccess.Seconds + " seconden geleden";
-            }
-
-            builder.AppendLine(appendTime);
+            builder.AppendLine(tijdSindsAccess);
             return builder.ToString();
+        }
+
+       public readonly string tijdSindsAccess {
+            get
+            {
+                TimeSpan dateAccess = DateTime.Now.Subtract(fileInfo.LastAccessTime);
+                string appendTime = "";
+
+                if (dateAccess.Days > 0)
+                {
+                    appendTime = dateAccess.Days + " dagen geleden";
+                }
+                else if (dateAccess.Hours > 0)
+                {
+                    appendTime = dateAccess.Hours + " u(u)r(en) geleden";
+                }
+                else if (dateAccess.Minutes > 0)
+                {
+                    appendTime = dateAccess.Minutes + " minuten geleden";
+                }
+                else
+                {
+                    appendTime = dateAccess.Seconds + " seconden geleden";
+                }
+                return appendTime;
+            }
         }
     }
 }
---
+++ Les1-3-dll/deel3/Maand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Les1_3_dll.deel3
+{
+    public class Maand
+    {
+        public int Maandnr { get; set; }
+        public int Jaar { get; set; }
+
+        public Maand()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            DateTime dt = new DateTime(Jaar, Maandnr, 1);
+            sb.AppendLine(dt.Month + " " + dt.Year);
+
+
+            return sb.ToString();
+        }
+    }
+}
