0b8b973eb13b3aa7d8494a57ae279c4e13f348fc
Dries Christiaens <dries.christiaens@student.Ehb.Be>
2014-03-04 14:30:28 +0100

    Oef 2

--- WanteLes1-2/Program.cs
+++ WanteLes1-2/Program.cs
@@ -2,13 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace WanteLes1_2
 {
+    class EmailProcessor
+    {
+        public delegate void EmailReceived(string email);
+        public event EmailReceived EmailReceivedEvent;
+
+        public void checkEmail()
+        {
+            while(true)
+            {
+                if(checkServer())
+                {
+                    EmailReceivedEvent("some email");
+                }
+                Thread.Sleep(5000);
+            }
+        }
+
+        private bool checkServer()
+        {
+            return true;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            EmailProcessor emailProcessor = new EmailProcessor();
+            emailProcessor.EmailReceivedEvent += emailProcessor_EmailReceivedEvent;
+            emailProcessor.checkEmail();
+        }
+
+        static void emailProcessor_EmailReceivedEvent(string email)
+        {
+            Console.WriteLine("Email Received: {0}", email);
         }
     }
 }
